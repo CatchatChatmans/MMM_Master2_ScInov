6c22260bca3c66bcfc446f20a3a091ccc7a4561b
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-12-05 00:13:32 +0100

    Continued the implementation of the LiveData-ViewModel architecture

---
+++ app/src/main/java/com/example/workstation/scinov/EventViewModel.java
@@ -0,0 +1,21 @@
+package com.example.workstation.scinov;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EventViewModel extends ViewModel {
+    private static final DatabaseReference EVENT_REF =
+            FirebaseDatabase.getInstance().getReference("/events");
+
+    private final FirebaseQueryLiveData liveData = new FirebaseQueryLiveData(EVENT_REF);
+
+    @NonNull
+    public LiveData<DataSnapshot> getDataSnapshotLiveData() {
+        return liveData;
+    }
+}
