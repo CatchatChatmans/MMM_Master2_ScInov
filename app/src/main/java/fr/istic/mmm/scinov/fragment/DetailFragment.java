73118c55027dcd066f58d0804a7e40b0976abf3e
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-12-30 00:16:04 +0100

    started adding details in the details fragment

--- app/src/main/java/fr/istic/mmm/scinov/fragment/DetailFragment.java
+++ app/src/main/java/fr/istic/mmm/scinov/fragment/DetailFragment.java
@@ -2,18 +2,25 @@ package fr.istic.mmm.scinov.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import fr.istic.mmm.scinov.R;
+import fr.istic.mmm.scinov.model.Event;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
 
+    private Event event;
+    private TextView eventName;
+    private TextView eventTheme;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -27,4 +34,16 @@ public class DetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        eventName = view.findViewById(R.id.detailsName);
+        eventTheme = view.findViewById(R.id.detailsTheme);
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            event = bundle.getParcelable("Event");
+            eventName.setText(event.getName());
+            eventTheme.setText(event.getTheme());
+        }
+    }
 }
