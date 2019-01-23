6fcc4e1f08b29bb992a55b5d5dcb3e0458d21699
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2019-01-24 00:52:31 +0100

    In the EventDetailFragment, added an observer of the liveData pulled from the viewModel of the main activity in order to avoid reloading of the data when going back (to improve)

--- app/src/main/java/fr/istic/mmm/scinov/activities/EventDetail/EventDetailFragment.java
+++ app/src/main/java/fr/istic/mmm/scinov/activities/EventDetail/EventDetailFragment.java
@@ -1,14 +1,21 @@
 package fr.istic.mmm.scinov.activities.EventDetail;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import fr.istic.mmm.scinov.R;
 import fr.istic.mmm.scinov.model.Event;
+import fr.istic.mmm.scinov.model.EventViewModel;
 
 public class EventDetailFragment extends Fragment {
 
@@ -43,27 +50,16 @@ public class EventDetailFragment extends Fragment {
         .addToBackStack(null)
         .commit();
 
+        // Observe the live data from the view model of the main activity to avoid reloading the data when going back
+        EventViewModel viewModel = ViewModelProviders.of(getActivity()).get(EventViewModel.class);
+        LiveData<List<Event>> liveData = viewModel.getEventsLiveData();
+        liveData.observe(this, new Observer<List<Event>>() {
+            @Override
+            public void onChanged(@Nullable List<Event> events) {
+
+            }
+        });
+
         return inflater.inflate(R.layout.activity_event_detail, container, false);
     }
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//
-////            DetailNestedFragment detailFragment = new DetailNestedFragment();
-////            detailFragment.setArguments(getIntent().getExtras());
-////
-////            MapNestedFragment mapFragment = new MapNestedFragment();
-////            mapFragment.setArguments(getIntent().getExtras());
-////
-////            FragmentManager fragmentManager = getSupportFragmentManager();
-////            fragmentManager.beginTransaction()
-////            .add(R.id.detailFrame, detailFragment)
-////            .add(R.id.mapFrame, mapFragment)
-////            .commit();
-//
-//    }
-
 }
