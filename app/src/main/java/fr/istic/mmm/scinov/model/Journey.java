53b65477ff6c0c4189091d4f518f350b478df630
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2019-01-24 18:45:57 +0100

    View Model des journeys + journey object

--- app/src/main/java/fr/istic/mmm/scinov/model/Journey.java
+++ app/src/main/java/fr/istic/mmm/scinov/model/Journey.java
@@ -3,12 +3,16 @@ package fr.istic.mmm.scinov.model;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.util.List;
+
 @IgnoreExtraProperties
 public class Journey {
 
     private String id;
     private String name;
-    private String type;
+    private Boolean shared;
+    private String author;
+    private List<String> observers;
 
     public Journey() {
     }
---
+++ app/src/main/java/fr/istic/mmm/scinov/model/JourneyViewModel.java
@@ -0,0 +1,56 @@
+package fr.istic.mmm.scinov.model;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class JourneyViewModel extends ViewModel {
+    private static final Query JOURNEY_QUERY =
+            FirebaseDatabase.getInstance().getReference("/parcours");
+
+    private FirebaseQueryLiveData liveData = new FirebaseQueryLiveData(JOURNEY_QUERY);
+
+    private FirebaseQueryLiveData queriedLiveData;
+
+    private final MediatorLiveData<List<Journey>> journeysLiveData = new MediatorLiveData<>();
+
+    private boolean isQueryActivated = false;
+
+    public JourneyViewModel() {
+
+        loadJourneys(liveData);
+
+    }
+
+    @NonNull
+    public LiveData<List<Journey>> getJourneysLiveData() {
+        return journeysLiveData;
+    }
+
+    private void loadJourneys(FirebaseQueryLiveData liveData) {
+        journeysLiveData.addSource(liveData, dataSnapshot -> {
+            if (dataSnapshot != null) {
+                new Thread(() -> {
+                    List<Journey> journeys = new LinkedList<>();
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        journeys.add(child.getValue(Journey.class));
+                    }
+                    journeysLiveData.postValue(journeys);
+                }).start();
+            } else {
+                journeysLiveData.setValue(null);
+            }
+        });
+    }
+
+}
