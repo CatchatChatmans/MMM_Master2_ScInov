51d4b7497aad69febc530398ad0c09dbed1f9b3b
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2019-01-24 19:08:05 +0100

    Journey rendu Parcelable

--- app/src/main/java/fr/istic/mmm/scinov/model/Journey.java
+++ app/src/main/java/fr/istic/mmm/scinov/model/Journey.java
@@ -1,19 +1,88 @@
 package fr.istic.mmm.scinov.model;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 
-import java.util.List;
 
 @IgnoreExtraProperties
-public class Journey {
+public class Journey implements Parcelable {
 
     private String id;
-    private String name;
+    private String title;
     private Boolean shared;
     private String author;
-    private List<String> observers;
 
     public Journey() {
     }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getShared() {
+        return shared;
+    }
+
+    public void setShared(Boolean shared) {
+        this.shared = shared;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    protected Journey(Parcel in) {
+        id = in.readString();
+        title = in.readString();
+        author = in.readString();
+        shared = in.readByte() != 0;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(this.id);
+        dest.writeString(this.title);
+        dest.writeString(this.author);
+        dest.writeByte((byte) (this.shared ? 1 : 0));
+
+    }
+
+    public static final Creator<Journey> CREATOR = new Creator<Journey>() {
+        @Override
+        public Journey createFromParcel(Parcel in) {
+            return new Journey(in);
+        }
+
+        @Override
+        public Journey[] newArray(int size) {
+            return new Journey[size];
+        }
+    };
+
 }
